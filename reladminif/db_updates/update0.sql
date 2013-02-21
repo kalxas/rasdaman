62e430ca3875919288fcb95aa7df8a3371c1c5f4
Dimitar Misev <misev@rasdaman.com>
2013-02-21 23:45:07 +0100

    Fix column names in database updates table

--- reladminif/db_updates/update0.sql
+++ reladminif/db_updates/update0.sql
@@ -1,10 +1,20 @@
 -- table storing latest database update
-
+                                                                                                                
+CREATE OR REPLACE function create_dbupdates_table() returns void as $$
 CREATE TABLE ras_dbupdates (
   id serial NOT NULL,
-  update integer,
+  UpdateNumber INTEGER,
+  UpdateNumberRe INTEGER,
   primary key (id)
 );
 
-insert into ras_dbupdates values (1, 0);
+insert into ras_dbupdates values (1, 0, 0);
+$$ language sql;
 
+SELECT CASE WHEN (
+  SELECT COUNT(*)
+  FROM information_schema.tables
+  WHERE table_name = 'ras_dbupdates') = 0
+THEN create_dbupdates_table() END;
+
+DROP function create_dbupdates_table();
