469260e033fc74a17b46646b9e9cb1d80a659f42
Vlad M <vlad@flanche.net>
2013-03-07 17:06:34 +0100

    cleared the playground cached files in raswct

--- applications/raswct/apps/playground/files/8usjmqnn0slmum8cotdn4po911/userCode.js
+++
@@ -1,2 +0,0 @@
-var dataSeries = new Rj.util.DataSeries([1,5,2,4,10]);
-var widget = new Rj.widget.AreaDiagram("#area-widget", dataSeries);
--- applications/raswct/apps/playground/files/hv90eelj2hsa773b16mjj8efr5/userCode.js
+++
@@ -1,5 +0,0 @@
-var led = new Rj.widget.Led("#led-widget");
-setInterval(function(){
-    var value = Math.random()*100 + Math.random();
-    led.setValue(value);
-}, 2000);
