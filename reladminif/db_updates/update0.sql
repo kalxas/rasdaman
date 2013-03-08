2c6ba9cd8ea8895ad02979c68917c78b3ef86868
Dimitar Misev <misev@rasdaman.com>
2013-03-08 17:01:07 +0100

    Fix database update script (ticket #275)

--- reladminif/db_updates/update0.sql
+++ reladminif/db_updates/update0.sql
@@ -1,20 +1,23 @@
 -- table storing latest database update
-                                                                                                                
-CREATE OR REPLACE function create_dbupdates_table() returns void as $$
-CREATE TABLE ras_dbupdates (
-  id serial NOT NULL,
-  UpdateNumber INTEGER,
-  UpdateNumberRe INTEGER,
-  primary key (id)
-);
 
-insert into ras_dbupdates values (1, 0, 0);
-$$ language sql;
+CREATE FUNCTION create_table() RETURNS VOID AS
+$$
+BEGIN
+    if not exists(
+        select * from information_schema.tables 
+        where table_catalog = CURRENT_CATALOG and table_schema = CURRENT_SCHEMA
+              and table_name = 'ras_dbupdates') then
 
-SELECT CASE WHEN (
-  SELECT COUNT(*)
-  FROM information_schema.tables
-  WHERE table_name = 'ras_dbupdates') = 0
-THEN create_dbupdates_table() END;
+        CREATE TABLE RAS_DBUPDATES (
+		    UpdateType VARCHAR(5) NOT NULL,
+		    UpdateNumber INTEGER,
+		    primary key (UpdateType)
+		);
+        INSERT INTO RAS_DBUPDATES values ('rc', 0);
+    end if;
+END;
+$$
+LANGUAGE plpgsql;
 
-DROP function create_dbupdates_table();
+SELECT create_table();
+DROP function create_table();
