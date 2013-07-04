kalxas/rasdaman

commit 290f4fbaafaf12522b72ca651b7f2b73eb4a56a9

Tile cache, to be used during bulk ingestion only.

relblobif/blobtilecommon.cc
@@ -48,6 +48,8 @@ rasdaman GmbH.
 #include "raslib/error.hh"
 #include "reladminif/externs.h"
 #include "raslib/rmdebug.hh"
+#include "debug/debug-srv.hh"
+#include "reladminif/adminif.hh"
 #include "reladminif/sqlerror.hh"
 #include "tileid.hh"
 #include "inlinetile.hh"
@@ -164,6 +166,8 @@ BLOBTile::BLOBTile(const OId& id, r_Bytes newSize, r_Data_Format newFmt)
 BLOBTile::~BLOBTile()
 {
     RMDBGENTER(3, RMDebug::module_blobif, "BLOBTile", "~BLOBTile() " << myOId);
+    // unsigned long int addr = (unsigned long int) this;
+    // RMInit::logOut << "BLOBTile::~BLOBTile( "  << myOId << ", ptr = " << addr << " )" << endl;
     validate();
     RMDBGEXIT(3, RMDebug::module_blobif, "BLOBTile", "~BLOBTile() " << myOId << ")");
 }
@@ -171,3 +175,229 @@ BLOBTile::~BLOBTile()
 char*   BLOBTile::BLOBBuffer = NULL;
 r_Bytes BLOBTile::BLOBBufferLength = 131072;
 
+void
+BLOBTile::destroyReal() throw (r_Error)
+{
+    RMDBGENTER(3, RMDebug::module_blobif, "BLOBTile", "destroyReal() " << myOId);
+    if (cells)
+    {
+        TALK("BLOBTile::destroyReal() freeing blob cells");
+        free(cells);
+        cells = NULL;
+    }
+    RMDBGEXIT(3, RMDebug::module_blobif, "BLOBTile", "destroyReal() " << myOId << ")");
+}
+
+BLOBTile*
+BLOBTile::clone()
+{
+    BLOBTile* ret = new BLOBTile(dataFormat);
+    ret->_isCached = _isCached;
+    ret->_isInDatabase = _isInDatabase;
+    ret->_isModified = _isModified;
+    ret->_isPersistent = _isPersistent;
+    ret->cells = cells;
+    ret->currentFormat = currentFormat;
+    ret->myOId = myOId;
+    ret->referenceCount = referenceCount;
+    ret->size = size;
+    return ret;
+}
+
+void
+BLOBTile::from(BLOBTile* tile)
+{
+    _isCached = tile->_isCached;
+    _isInDatabase = tile->_isInDatabase;
+    _isModified = tile->_isModified;
+    _isPersistent = tile->_isPersistent;
+    cells = tile->cells;
+    currentFormat = tile->currentFormat;
+    myOId = tile->myOId;
+    referenceCount = tile->referenceCount;
+    size = tile->size;
+}
+
+
+//writes the object to database/deletes it or updates it.
+//a r_Error::r_Error_TransactionReadOnly is thrown when the transaction is readonly.
+void
+BLOBTile::validateReal() throw (r_Error)
+{
+    RMDBGENTER(9, RMDebug::module_adminif, "DBObject", "validate() " << myOId);
+    ENTER("BLOBTile::validateReal()");
+    if (_isModified)
+    {
+        if (!AdminIf::isReadOnlyTA())
+        {
+            if (!AdminIf::isAborted())
+            {
+                if (_isInDatabase)
+                {
+                    if (_isPersistent)
+                    {
+                        RMDBGMIDDLE(11, RMDebug::module_adminif, "DBObject", "is persistent and modified and in database");
+#ifdef RMANBENCHMARK
+                        updateTimer.resume();
+#endif
+                        this->updateInDbReal();
+#ifdef RMANBENCHMARK
+                        updateTimer.pause();
+#endif
+                    }
+                    else
+                    {
+                        RMDBGMIDDLE(11, RMDebug::module_adminif, "DBObject", "is not persistent and in database");
+#ifdef RMANBENCHMARK
+                        deleteTimer.resume();
+#endif
+                        this->deleteFromDbReal();
+                        this->destroyReal();
+#ifdef RMANBENCHMARK
+                        deleteTimer.pause();
+#endif
+                    }
+                }
+                else
+                {
+                    if (_isPersistent)
+                    {
+                        RMDBGMIDDLE(11, RMDebug::module_adminif, "DBObject", "is persistent and modified and not in database");
+
+#ifdef RMANBENCHMARK
+                        insertTimer.resume();
+#endif
+                        this->insertInDbReal();
+#ifdef RMANBENCHMARK
+                        insertTimer.pause();
+#endif
+                    }
+                    else
+                    {
+                        //do not do anything: not in db and not persistent
+                    }
+                }
+            }
+            else
+            {
+                //do not do anything: is aborted
+            }
+        }
+        else
+        {
+            //do not do anything: is read only
+        }
+    }
+    else
+    {
+        //do not do anything: not modified
+    }
+    LEAVE("BLOBTile::validateReal()");
+    RMDBGEXIT(9, RMDebug::module_adminif, "DBObject", "validate() " << myOId);
+}
+
+// -----------------------------------------------------------------------------
+
+
+// delete one tuple from ras_tiles table, update map ref
+// tuple is identified by blobtile.cc var myOId
+void
+BLOBTile::deleteFromDb() throw (r_Error)
+{
+    RMDBGENTER(3, RMDebug::module_blobif, "BLOBTile", "deleteFromDb() " << myOId);
+    ENTER( "BLOBTile::deleteFromDb" );
+    TileCache::removeKey(myOId);
+    deleteFromDbReal();
+    LEAVE( "BLOBTile::deleteFromDb, myOId=" << myOId );
+    RMDBGEXIT(3, RMDebug::module_blobif, "BLOBTile", "deleteFromDb() " << myOId);
+}
+
+// delete a range of tuple(s) from ras_tiles table, update map ref
+// tuples are identified by target and a range
+void
+BLOBTile::kill(const OId& target, unsigned int range)
+{
+    RMDBGENTER(0, RMDebug::module_blobif, "BLOBTile", "kill(" << target << ", " << range <<")");
+    ENTER( "BLOBTile::kill, target=" << target << ", range=" << range );
+    TileCache::removeKey(target);
+    killReal(target, range);
+    LEAVE( "BLOBTile::kill" );
+    RMDBGEXIT(0, RMDebug::module_blobif, "BLOBTile", "kill(" << target << " " << target.getType() << ")");
+}
+
+// insert new blob into ras_tiles table, update map ref
+// tuple is identified by blobtile.cc var myOId
+// data is taken from buffer 'cells' containing 'size' bytes
+void
+BLOBTile::insertInDb() throw (r_Error)
+{
+    RMDBGENTER(3, RMDebug::module_blobif, "BLOBTile", "insertInDb() " << myOId);
+    ENTER( "BLOBTile::insertInDb" );
+    if (TileCache::cacheLimit > 0)
+    {
+        TileCache::insert(myOId, this);
+    }
+    else
+    {
+        insertInDbReal();
+    }
+    LEAVE( "BLOBTile::insertInDb(), myOId=" << myOId );
+    RMDBGEXIT(3, RMDebug::module_blobif, "BLOBTile", "insertInDb() " << myOId);
+} // insertInDb()
+
+// update blob in ras_tiles table, identified by variable myOId (from blobtile.cc), update map ref
+void
+BLOBTile::updateInDb() throw (r_Error)
+{
+    RMDBGENTER(3, RMDebug::module_blobif, "BLOBTile", "updateInDb() " << myOId);
+    ENTER( "BLOBTile::updateInDb" );
+    
+    if (TileCache::cacheLimit > 0)
+    {
+        TileCache::insert(myOId, this);
+    }
+    else
+    {
+        updateInDbReal();
+    }
+    LEAVE( "BLOBTile::updateInDb(), myOId=" << myOId );
+    RMDBGEXIT(3, RMDebug::module_blobif, "BLOBTile", "updateInDb() " << myOId);
+} // insertInDb()
+
+// read with cache consideration
+void
+BLOBTile::readFromDb() throw (r_Error)
+{
+    RMDBGENTER(3, RMDebug::module_blobif, "BLOBTile", "readFromDb() " << myOId);
+    ENTER( "BLOBTile::readFromDb" );
+    
+    if (TileCache::cacheLimit > 0)
+    {
+        if (TileCache::contains(myOId))
+        {
+            BLOBTile* cachedTile = TileCache::get(myOId);
+            if (cachedTile != NULL)
+            {
+                from(cachedTile);
+                TileCache::update(myOId, this);
+            }
+            else
+            {
+                readFromDbReal();
+                TileCache::insert(myOId, this);
+            }
+        }
+        else
+        {
+            readFromDbReal();
+            TileCache::insert(myOId, this);
+        }
+    }
+    else
+    {
+        readFromDbReal();
+    }
+    
+    LEAVE( "BLOBTile::readFromDb" );
+    RMDBGEXIT(3, RMDebug::module_blobif, "BLOBTile", "readFromDb() " << myOId);
+}

relblobif/tilecache.cc
@@ -1,0 +1,327 @@
+/*
+* This file is part of rasdaman community.
+*
+* Rasdaman community is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Rasdaman community is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with rasdaman community.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009 Peter Baumann /
+rasdaman GmbH.
+*
+* For more information please see <http://www.rasdaman.org>
+* or contact Peter Baumann via <baumann@rasdaman.com>.
+*/
+
+#include "config.h"
+#include "tilecache.hh"
+#include "raslib/rmdebug.hh"
+#include "debug/debug-srv.hh"
+
+#include <algorithm>
+
+//#define DEBUG_CACHE
+
+#define OID_KEY(key) (key.getCounter())
+
+#ifdef DEBUG_CACHE
+    #define TENTER(msg) RMInit::logOut << "ENTER " << msg << endl;
+#else
+    #define TENTER(msg) ENTER(msg);
+#endif
+
+#ifdef DEBUG_CACHE
+    #define TLEAVE(msg) RMInit::logOut << "LEAVE " << msg << endl;
+#else
+    #define TLEAVE(msg) LEAVE(msg);
+#endif
+
+#ifdef DEBUG_CACHE
+    #define TTALK(msg) RMInit::logOut << "  " << msg << endl;
+#else
+    #define TTALK(msg) TALK(msg);
+#endif
+
+CacheType TileCache::cache;
+CacheLRU TileCache::lru;
+
+long TileCache::cacheLimit = 0L;
+long TileCache::cacheSize = 0L;
+
+bool TileCache::insert(KeyType key, ValueType value)
+{
+    if (key == 0)
+    {
+        // invalid key
+        return false;
+    }
+    
+    TENTER("TileCache::insert( " << key << " )");
+    
+    bool ret = false;
+    BLOBTile* tileToCache = value;
+    printBlob(tileToCache, "tile to cache");
+    if (!contains(key))
+    {
+        BLOBTile* cached = tileToCache->clone();
+        printBlob(cached, "cloned tile to be cached");
+        cache.insert(CachePairType(key, cached));
+        updateValue(cached);
+        cacheSize += cached->getSize();
+        readjustCache();
+        ret = true;
+    }
+    else
+    {
+        BLOBTile* tile = cache[key];
+        if (tile == NULL)
+        {
+            RMInit::logOut << "Error: cached NULL value!" << endl;
+            remove(key);
+        }
+        else if (tile == tileToCache)
+        {
+            BLOBTile* cloned = tileToCache->clone();
+            update(key, cloned, false);
+        }
+        else
+        {
+            tile->from(tileToCache);
+            printBlob(tile, "cached tile");
+            updateValue(tile);
+        }
+        TTALK("already inserted");
+    }
+    TLEAVE("TileCache::insert()");
+    return ret;
+}
+
+bool TileCache::update(KeyType key, ValueType value, bool deleteTile)
+{
+    if (key == 0)
+    {
+        return false;
+    }
+    TENTER("TileCache::update( " << key << " )");
+    
+    bool ret = false;
+    BLOBTile* tileToCache = value;
+    printBlob(tileToCache, "tile to cache");
+    if (!contains(key))
+    {
+        insert(key, value);
+    }
+    else
+    {
+        BLOBTile* tile = cache[key];
+        if (tile == NULL)
+        {
+            RMInit::logOut << "Error: cached NULL value!" << endl;
+            remove(key);
+        }
+        else
+        {
+            cache.erase(key);
+            removeValue(tile);
+            cacheSize -= tile->getSize();
+            printBlob(tile, "removed tile");
+            tile->_isModified = false;
+            if (deleteTile)
+            {
+                delete tile;
+            }
+            
+            cache.insert(CachePairType(key, tileToCache));
+            updateValue(tileToCache);
+            cacheSize += tileToCache->getSize();
+            readjustCache();
+        }
+        TTALK("already inserted");
+    }
+    TLEAVE("TileCache::update()");
+    return ret;
+}
+
+ValueType TileCache::get(KeyType key)
+{
+    TENTER("TileCache::get( " << key << " )");
+    BLOBTile* ret = NULL;
+    if (contains(key))
+    {
+        ret = cache[key];
+        if (ret != NULL)
+        {
+            updateValue(ret);
+        }
+    }
+    else
+    {
+        TTALK("key not found");
+    }
+    TLEAVE("TileCache::get()");
+    return ret;
+}
+
+bool TileCache::contains(KeyType key)
+{
+    return cache.find(key) != cache.end();
+}
+
+void TileCache::remove(KeyType key)
+{
+    TENTER("TileCache::remove( " << key << " )");
+    if (contains(key))
+    {
+        BLOBTile* tile = cache[key];
+        if (tile != NULL)
+        {
+            printBlob(tile, "tile to remove from cache");
+            cacheSize -= tile->getSize();
+            removeValue(tile);
+            tile->validateReal();
+            delete tile;
+        }
+        cache.erase(key);
+    }
+    else
+    {
+        TTALK("key not found");
+    }
+    TLEAVE("TileCache::remove()");
+}
+
+void TileCache::removeKey(KeyType key)
+{
+    TENTER("TileCache::remove( " << key << " )");
+    if (contains(key))
+    {
+        BLOBTile* tile = cache[key];
+        if (tile != NULL)
+        {
+            printBlob(tile, "tile to remove from cache");
+            cacheSize -= tile->getSize();
+            removeValue(tile);
+        }
+        cache.erase(key);
+    }
+    else
+    {
+        TTALK("key not found");
+    }
+    TLEAVE("TileCache::remove()");
+}
+
+void TileCache::clear()
+{
+    TENTER("TileCache::clear() - clear cache of size: " << cache.size());
+    
+    typedef CacheType::iterator it_type;
+    for (it_type it = cache.begin(); it != cache.end(); it++)
+    {
+        TTALK("TileCache::clear() - removing key " << it->first);
+        BLOBTile* tile = it->second;
+        tile->validateReal();
+        delete tile;
+    }
+    cache.clear();
+    lru.clear();
+    cacheSize = 0;
+    TLEAVE("TileCache::clear()");
+}
+
+void TileCache::readjustCache()
+{
+    TENTER("TileCache::readjustCache( cache size = " << cacheSize << ", cache limit = " << cacheLimit << " )"); 
+    if (cacheSize > cacheLimit)
+    {
+        long count = 0;
+        TTALK("freeing up space from cache...");
+        while (cacheSize > cacheLimit && lru.size() > 0)
+        {
+            BLOBTile* tile = lru.back();
+            lru.pop_back();
+            remove(tile->myOId);
+            ++count;
+        }
+        TTALK("removed " << count << " blobs from cache.");
+    }
+    TLEAVE("TileCache::readjustCache()");  
+}
+
+void TileCache::updateValue(ValueType value)
+{
+    CacheLRU::iterator pos = std::find(lru.begin(), lru.end(), value);
+    if (pos != lru.end())
+    {
+        lru.splice(lru.begin(), lru, pos);
+    }
+    else
+    {
+        lru.insert(lru.begin(), value);
+    }
+}
+
+void TileCache::removeValue(ValueType value)
+{
+    CacheLRU::iterator pos = std::find(lru.begin(), lru.end(), value);
+    if (pos != lru.end())
+    {
+        lru.erase(pos);
+    }
+}
+
+bool TileCache::insert(OId& key, ValueType value)
+{
+    return insert(OID_KEY(key), value);
+}
+
+bool TileCache::update(OId& key, ValueType value)
+{
+    return update(OID_KEY(key), value);
+}
+
+ValueType TileCache::get(OId& key)
+{
+    return get(OID_KEY(key));
+}
+
+bool TileCache::contains(OId& key)
+{
+    return contains(OID_KEY(key));
+}
+
+void TileCache::remove(OId& key)
+{
+    remove(OID_KEY(key));
+}
+
+void TileCache::removeKey(OId& key)
+{
+    removeKey(OID_KEY(key));
+}
+
+void
+TileCache::printBlob(BLOBTile* tile, char *msg)
+{
+#ifdef DEBUG_CACHE
+    unsigned long int addr = (unsigned long int) tile;
+    
+    RMInit::logOut << " -> " << msg << endl;
+    RMInit::logOut << "    ptr        " << addr << endl;
+    RMInit::logOut << "    oid        " << tile->myOId << endl;
+    RMInit::logOut << "    size       " << tile->size << endl;
+    RMInit::logOut << "    modified   " << tile->_isModified << endl;
+    RMInit::logOut << "    cached     " << tile->_isCached << endl;
+    RMInit::logOut << "    in db      " << tile->_isInDatabase << endl;
+    RMInit::logOut << "    persistent " << tile->_isPersistent << endl;
+    RMInit::logOut << "" << endl;
+#endif
+}

configure.ac
@@ -108,6 +108,7 @@ AC_CONFIG_HEADERS([config.h])
 AC_CONFIG_FILES([Makefile
                  catalogmgr/Makefile         
                  relblobif/Makefile
+                 relblobif/test/Makefile
                  relindexif/Makefile
                  relmddif/Makefile
                  relcatalogif/Makefile

qlparser/lex.ll
@@ -207,6 +207,7 @@ int string_yyinput( char* buf, int max_size )
 "in"                                     { SETTOKEN( IN, commandToken, IN ) }
 "marray"                                 { SETTOKEN( MARRAY, commandToken, MARRAY ) }
 "condense"                               { SETTOKEN( CONDENSE, commandToken, CONDENSE ) }
+"commit"                                 { SETTOKEN( COMMIT, commandToken, COMMIT ) }
 
 "oid"                                    { SETTOKEN( OID, commandToken, OID ) }
 "shift"                                  { SETTOKEN( SHIFT, commandToken, SHIFT ) }

qlparser/oql.yy
@@ -215,7 +215,7 @@ struct QtUpdateSpecElement
 			 WITH SUBTILING AREA OF INTEREST STATISTIC TILE SIZE BORDER THRESHOLD
 			 STRCT COMPLEX RE IM TIFF BMP HDF NETCDF CSV JPEG PNG VFF TOR DEM INV_TIFF INV_BMP INV_HDF INV_NETCDF
 			 INV_JPEG INV_PNG INV_VFF INV_CSV INV_TOR INV_DEM ENCODE CONCAT ALONG DBINFO
-                         CASE WHEN THEN ELSE END
+                         CASE WHEN THEN ELSE END COMMIT
 
 %left COLON VALUES USING WHERE
 %left OVERLAY
@@ -250,7 +250,7 @@ struct QtUpdateSpecElement
 %type <integerToken>          intLitExp
 %type <operationValue>        condenseOpLit 
 %type <castTypes>	      castType
-%type <dummyValue>            qlfile query selectExp createExp insertExp deleteExp updateExp dropExp selectIntoExp 
+%type <dummyValue>            qlfile query selectExp createExp insertExp deleteExp updateExp dropExp selectIntoExp commitExp
 //%type <identifierToken>       namedCollection collectionIterator typeName attributeIdent pyrName 
 %type <identifierToken>       namedCollection collectionIterator typeName attributeIdent
 			      marrayVariable condenseVariable
@@ -289,9 +289,34 @@ query: createExp
 	| updateExp
 	| insertExp
 	| deleteExp
+    | commitExp
 //        | pyramidExp;
 	;
 
+commitExp: COMMIT
+{
+	  try {
+	    accessControl.wantToWrite();
+	  }
+	    catch(...) {
+	    // save the parse error info and stop the parser
+            if ( parseError ) delete parseError;
+            parseError = new ParseInfo( 803, $1.info->getToken().c_str(),
+                                        $1.info->getLineNo(), $1.info->getColumnNo() );
+	    FREESTACK($1)
+	    QueryTree::symtab.wipe();
+            YYABORT;
+	  }
+
+	  // create the command node
+	  QtCommand* commandNode = new QtCommand( QtCommand::QT_COMMIT, "dummy" );
+	  commandNode->setParseInfo( *($1.info) );
+	  
+	  // set insert node  as root of the Query Tree
+	  parseQueryTree->setRoot( commandNode );
+	  
+	  FREESTACK($1)
+}
 
 createExp: CREATE COLLECTION namedCollection typeName
 	{

qlparser/qtcommand.cc
@@ -42,6 +42,7 @@ static const char rcsid[] = "@(#)qlparser, QtCommand: $Header: /home/rasdev/CVS-
 #include "servercomm/servercomm.hh"
 #include "qlparser/querytree.hh"
 #include "qlparser/qtinsert.hh"
+#include "relblobif/tilecache.hh"
 
 #include <iostream>
 
@@ -284,6 +285,9 @@ QtCommand::evaluate()
         break;
     case QT_CREATE_COLLECTION:
         createCollection(collectionName, typeName);
+        break;
+    case QT_COMMIT:
+        TileCache::clear();
         break;
     case QT_CREATE_COLLECTION_FROM_QUERY_RESULT:
 

qlparser/qtcommand.hh
@@ -54,7 +54,8 @@ public:
     {
         QT_DROP_COLLECTION,
         QT_CREATE_COLLECTION,
-        QT_CREATE_COLLECTION_FROM_QUERY_RESULT
+        QT_CREATE_COLLECTION_FROM_QUERY_RESULT,
+        QT_COMMIT
     };
 
     /// constructor getting command, collection and type name (create collection)

qlparser/qtconst.cc
@@ -162,7 +162,7 @@ QtConst::optimizeLoad( QtTrimList* trimList )
 QtData*
 QtConst::evaluate( QtDataList* /*inputList*/ )
 {
-    RMDBCLASS( "QtConst", "evaluate( QtDataList* )", "qlparser", __FILE__, __LINE__ )
+//    RMDBCLASS( "QtConst", "evaluate( QtDataList* )", "qlparser", __FILE__, __LINE__ )
     startTimer("QtConst");
 
     QtData* returnValue = NULL;

commit 6515e1aaf0f8f18447ccf83172a840984b68fdeb

ticket:413 - fix memory leak introduced by commit 290f4fb

relblobif/blobtilecommon.cc
@@ -166,9 +166,15 @@ BLOBTile::BLOBTile(const OId& id, r_Bytes newSize, r_Data_Format newFmt)
 BLOBTile::~BLOBTile()
 {
     RMDBGENTER(3, RMDebug::module_blobif, "BLOBTile", "~BLOBTile() " << myOId);
-    // unsigned long int addr = (unsigned long int) this;
-    // RMInit::logOut << "BLOBTile::~BLOBTile( "  << myOId << ", ptr = " << addr << " )" << endl;
-    validate();
+    if (TileCache::cacheLimit == 0)
+    {
+        validateReal();
+        destroyReal();
+    }
+    else
+    {
+        validate();
+    }
     RMDBGEXIT(3, RMDebug::module_blobif, "BLOBTile", "~BLOBTile() " << myOId << ")");
 }
 

relblobif/tilecache.cc
@@ -187,6 +187,7 @@ void TileCache::remove(KeyType key)
             cacheSize -= tile->getSize();
             removeValue(tile);
             tile->validateReal();
+            tile->destroyReal();
             delete tile;
         }
         cache.erase(key);
@@ -229,6 +230,7 @@ void TileCache::clear()
         TTALK("TileCache::clear() - removing key " << it->first);
         BLOBTile* tile = it->second;
         tile->validateReal();
+        tile->destroyReal();
         delete tile;
     }
     cache.clear();
@@ -242,16 +244,17 @@ void TileCache::readjustCache()
     TENTER("TileCache::readjustCache( cache size = " << cacheSize << ", cache limit = " << cacheLimit << " )"); 
     if (cacheSize > cacheLimit)
     {
-        long count = 0;
-        TTALK("freeing up space from cache...");
-        while (cacheSize > cacheLimit && lru.size() > 0)
-        {
-            BLOBTile* tile = lru.back();
-            lru.pop_back();
-            remove(tile->myOId);
-            ++count;
-        }
-        TTALK("removed " << count << " blobs from cache.");
+        clear();
+//        long count = 0;
+//        TTALK("freeing up space from cache...");
+//        while (cacheSize > cacheLimit && lru.size() > 0)
+//        {
+//            BLOBTile* tile = lru.back();
+//            lru.pop_back();
+//            remove(tile->myOId);
+//            ++count;
+//        }
+//        TTALK("removed " << count << " blobs from cache.");
     }
     TLEAVE("TileCache::readjustCache()");  
 }