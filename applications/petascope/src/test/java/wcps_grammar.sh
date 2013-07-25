92f65cc19e8beac48915e458ccda7d79c9620421
Piero Campalani <p.campalani@jacobs-university.de>
2013-07-25 12:45:41 +0200

    ticket:421 - Remove ANTLR older jar library [II].

--- applications/petascope/src/test/java/wcps_grammar.sh
+++ applications/petascope/src/test/java/wcps_grammar.sh
@@ -41,6 +41,6 @@ if [ $# != 1 ]; then
 	exit 1
 fi
 
-CLASSPATH=.:../lib/wcps-all.jar:../lib/jaxb-api.jar:../lib/jaxb-impl.jar:../lib/postgresql-8.3-604.jdbc4.jar:../lib/jsr173_api.jar:../lib/rasj.jar:../lib/commons-math-1.1.jar:../lib/antlrworks-1.2.3.jar
+CLASSPATH=.:../lib/wcps-all.jar:../lib/jaxb-api.jar:../lib/jaxb-impl.jar:../lib/postgresql-8.3-604.jdbc4.jar:../lib/jsr173_api.jar:../lib/rasj.jar:../lib/commons-math-1.1.jar:../lib/antlrworks-1.3.1.jar
 
 java -cp $CLASSPATH wcps.server.cli.grammar "$1"
