74d94002ba66f49dd94a438a1d49516470874be8
BangPH <b.phamhuu@jacobs-university.de>
2016-02-17 06:42:34 +0100

    ticket:1254 - Add license header for WCPS packages
    Summary: Add license header for not autogenerated class in WCPS packages.
    
    Test Plan: No test.
    
    Reviewers: dmisev, vmerticariu
    
    Differential Revision: http://codereview.rasdaman.org/D147

--- applications/petascope/src/main/java/petascope/wcps2/cli/Wcps.java
+++ applications/petascope/src/main/java/petascope/wcps2/cli/Wcps.java
@@ -1,17 +1,34 @@
+/*
+ * This file is part of rasdaman community.
+ *
+ * Rasdaman community is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rasdaman community is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public License
+ * along with rasdaman community.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2003 - 2016 Peter Baumann / rasdaman GmbH.
+ *
+ * For more information please see <http://www.rasdaman.org>
+ * or contact Peter Baumann via <baumann@rasdaman.com>.
+ */
 package petascope.wcps2.cli;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import petascope.exceptions.rasdaman.RasdamanException;
-import petascope.util.Pair;
 import petascope.util.ras.RasQueryResult;
 import petascope.util.ras.RasUtil;
 import petascope.wcps2.error.managed.processing.WCPSProcessingError;
 import petascope.wcps2.translator.WcpsTranslator;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * This class provides a method to run wcps queries directly from the cli. Useful for testing and debugging
--- applications/petascope/src/main/java/petascope/wcps2/parser/wcpsEvaluator.java
+++ applications/petascope/src/main/java/petascope/wcps2/parser/wcpsEvaluator.java
@@ -1,10 +1,29 @@
+/*
+ * This file is part of rasdaman community.
+ *
+ * Rasdaman community is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rasdaman community is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public License
+ * along with rasdaman community.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2003 - 2016 Peter Baumann / rasdaman GmbH.
+ *
+ * For more information please see <http://www.rasdaman.org>
+ * or contact Peter Baumann via <baumann@rasdaman.com>.
+ */
 package petascope.wcps2.parser;
 
 import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.TerminalNode;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
-import petascope.wcps.grammar.CoverageExpr;
 import petascope.wcps2.error.managed.processing.InvalidAxisNameException;
 import petascope.wcps2.error.managed.processing.InvalidSubsettingException;
 import petascope.wcps2.metadata.CoverageRegistry;
