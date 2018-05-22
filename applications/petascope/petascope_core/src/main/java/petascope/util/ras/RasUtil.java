f38774b105be4ab37c63c36b946337abba02a190
Bang Pham Huu <b.phamhuu@jacobs-university.de>
2018-05-22 07:08:10 +0200

    ticket:1770 - Petascope should contain rasql error message in exception for invalid queries
    Summary: Petascope doesn't contain rasql error message for invalid Rasql queries and it is difficult to debug the real error without checking error in rasservers' logs.
    
    Test Plan: Run the test for WCPS query
    
    Reviewers: dmisev, vmerticariu
    
    Differential Revision: http://codereview.rasdaman.org/D605

--- applications/petascope/petascope_core/src/main/java/petascope/util/ras/RasUtil.java
+++ applications/petascope/petascope_core/src/main/java/petascope/util/ras/RasUtil.java
@@ -153,7 +153,7 @@ public class RasUtil {
                         throw new RasdamanCollectionDoesNotExistException(ExceptionCode.CollectionDoesNotExist, query, ex);
                     } else {
                         throw new RasdamanException(ExceptionCode.RasdamanRequestFailed,
-                                "Error evaluating rasdaman query: '" + query + "'", ex);
+                                "Error evaluating rasdaman query: '" + query + "'. Reason: " + ex.getMessage(), ex);
                     }
                 } catch (Error ex) {
                     tr.abort();
