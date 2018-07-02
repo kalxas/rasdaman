30731ff5d058c4ab1035db95992dee2691d2a1cf
Dragi Kamov <d.kamov@jacobs-university.de>
2018-07-02 13:07:15 +0200

    ticket:1440 - The systemtest should test compiling example C++
    Summary: The systemtest should test compiling example C++
    
    Test Plan: ./test.sh
    
    Reviewers: dmisev
    
    Differential Revision: http://codereview.rasdaman.org/D655

---
+++ systemtest/testcases_mandatory/test_example/test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# This file is part of rasdaman community.
+#
+# Rasdaman community is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Rasdaman community is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with rasdaman community. If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009 Peter Baumann /
+# rasdaman GmbH.
+#
+# For more information please see <http://www.rasdaman.org>
+# or contact Peter Baumann via <baumann@rasdaman.com>.
+#
+# SYNOPSIS
+#	test.sh
+# Description
+#	Command-line utility for testing rasdaman.
+#	1)Testing Makefile for C++
+#
+# Usage: ./test.sh
+#
+# CHANGE HISTORY
+#    2018-Jul-02   D.Kamov    created
+
+# Variables
+PROG=`basename $0`
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+. "$SCRIPT_DIR"/../../util/common.sh
+
+logn "testing Makefile for C++... "
+mkdir -p $SCRIPT_DIR/output
+cd "$RMANHOME/share/rasdaman/examples/c++/"
+make &>"$SCRIPT_DIR/output/make_cpp.log"
+check
+
+# ------------------------------------------------------------------------------
+# test summary
+#
+print_summary
+exit $RC
