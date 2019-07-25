296b5e55fe7bdcc43ff10662fe17567558e9a01e
Bang Pham Huu <b.phamhuu@jacobs-university.de>
2019-07-25 16:36:25 +0200

    ticket:2144 - CMake build gdal java version 2.x if it exists
    Summary: Use gdal java version 2.x if it exists (e.g: In Ubuntu 18.04).
    
    Test Plan: Tested manually
    
    Reviewers: dmisev
    
    Differential Revision: http://codereview.rasdaman.org/D922

--- cmake/modules/FindGdalJava.cmake
+++ cmake/modules/FindGdalJava.cmake
@@ -26,26 +26,27 @@
 
 include(FindPackageHandleStandardArgs)
 
-# try to find version 1.x
-find_library(GDAL_JAVA_VERSION_1 NAMES libgdaljni.so
+# try to find version 2.x
+find_library(GDAL_JAVA_VERSION_2 NAMES libgdalalljni.so.20 libgdaljni.so.20
              PATHS /usr/lib/java/gdal /usr/lib/jni/)
 
-if (GDAL_JAVA_VERSION_1)
-    # found version 1.x
-    get_filename_component(GDAL_JAVA_DIR ${GDAL_JAVA_VERSION_1} DIRECTORY)
+if (GDAL_JAVA_VERSION_2)
+    get_filename_component(GDAL_JAVA_DIR ${GDAL_JAVA_VERSION_2} DIRECTORY)
     set(GDAL_JAVA_DIR ${GDAL_JAVA_DIR})
-    set(GDAL_JAVA_VERSION "1.10.1")
-else ()
-    # try to find version 2.x
-    find_library(GDAL_JAVA_VERSION_2 NAMES libgdalalljni.so
+    set(GDAL_JAVA_VERSION "2.3.0")
+else()
+    # try to find version 1.x
+    find_library(GDAL_JAVA_VERSION_1 NAMES libgdaljni.so
                  PATHS /usr/lib/java/gdal /usr/lib/jni/)
 
-    if (GDAL_JAVA_VERSION_2)
-        get_filename_component(GDAL_JAVA_DIR ${GDAL_JAVA_VERSION_2} DIRECTORY)
+    if (GDAL_JAVA_VERSION_1)
+        # found version 1.x
+        get_filename_component(GDAL_JAVA_DIR ${GDAL_JAVA_VERSION_1} DIRECTORY)
         set(GDAL_JAVA_DIR ${GDAL_JAVA_DIR})
-        set(GDAL_JAVA_VERSION "2.3.0")
-    endif()    
-endif()
+        set(GDAL_JAVA_VERSION "1.10.1")
+    endif()
+endif()  
+
 
 # throw error on REQUIRED if GDAL_JAVA_DIR is not found, otherwise print found
 find_package_handle_standard_args(GdalJava DEFAULT_MSG GDAL_JAVA_DIR GDAL_JAVA_VERSION)
